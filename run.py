8590337e718450f54f2aeb5e0a2566bb8909d307
winnie <1002515603@qq.com>
2023-08-11 10:09:05 +0800

    更新allure_result文件夹name

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import pytest
 
 if __name__ == '__main__':
-    pytest.main(['-vs', '--alluredir=./result', '--junit-xml=./junit-result/result.xml'])
+    pytest.main(['-vs', '--alluredir=./allure-results', '--junit-xml=./junit-result/result.xml'])
