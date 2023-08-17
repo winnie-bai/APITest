95ea68015ebda7b070c40b8a4efc497789f42c65
winnie <1002515603@qq.com>
2023-08-17 12:12:53 +0800

    add Jenkinsfile and update db connect info

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+pipeline{
+    agent {
+        label 'ubuntu'
+    }
+    stage('api_test') {
+        steps {
+            sh '''cd $WORKSPACE
+                  python3 -m pip install -r requirements.txt
+                  python3 run.py'''
+            allure includeProperties: false, jdk: '', results: [[path: '$WORKSPACE/allure-results']]
+            archiveArtifacts artifacts: 'logs/*', followSymlinks: false
+        }
+    }
+}
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,7 @@
 import pymysql
+from pymysql import cursors
 
+from utils.log_utils import logger
 from utils.yaml_utils import open_yaml
 
 
@@ -11,16 +13,43 @@ def get_connect():
     db_info = open_yaml('test.yaml')['db_config']
     conn = pymysql.connect(host=db_info['host'], port=db_info['port'],
                            user=db_info['user'], password=db_info['password'],
-                           database=db_info['database'], charset=db_info['charset'])
+                           database=db_info['database'], charset=db_info['charset'],
+                           cursorclass=cursors.DictCursor)
+    conn.autocommit(True)
     return conn
 
 
 def excute_sql(sql):
-    connect = get_connect()
-    cursor = connect.cursor()
-    cursor.execute(sql)
-    record = cursor.fetchall()
-    # record = cursor.fetchone()
-    return record
+    '''
+    :param sql: 待执行的SQL语句
+    :return:
+    '''
+    try:
+        connect = get_connect()
+        cursor = connect.cursor()
+        cursor.execute(sql)
+        record = cursor.fetchall()
+        cursor.close()
+        connect.close()
+        if len(record) == 1:
+            return record[0]
+        elif len(record) == 0:
+            return None
+        else:
+            return record
+    except Exception as e:
+        logger.error(e)
+        return e
 
+# print(excute_sql("select * from student_info where student_name='winnie'"))
+
+# res = excute_sql("select * from student_info where student_name='winnie'")
+# print(res)
+# 添加语句
+# print(excute_sql("insert into student_info(student_name) values('test')"))
+# 删除语句
+# print(excute_sql("delete from student_info where id=7"))
+# 修改语句
+# print(excute_sql("update student_info set student_name='hi' where student_name='hello'"))
+# # 查询语句
 # print(excute_sql("select * from student_info"))
